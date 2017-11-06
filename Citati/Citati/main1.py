890c3c6c1e81ebc6875344f547a3372dd8de1a05
Mateja90 <32160869+Mateja90@users.noreply.github.com>
2017-11-06 16:45:20 +0100

    Popravek v izpisu

---
+++ Citati/Citati/main1.py
@@ -0,0 +1,14 @@
+import urllib
+
+from BeautifulSoup import BeautifulSoup
+
+
+topic_url = 'http://quotes.yourdictionary.com/theme/marriage/'
+topic_html = urllib.urlopen(topic_url).read()
+topic_soup = BeautifulSoup(topic_html)
+
+# find all paragraphs that have a class "quoteContent"
+quotes = topic_soup.findAll('p', attrs={'class': 'quoteContent'})
+
+for quote in quotes:
+    print quote.text
